cb7f9c8bee1c12bd5aaeda1f84e27e18cf57f85c
Aleksei Zhuchkov <kelzzzer@gmail.com>
2024-11-03 15:46:52 +0300

    feat: add config for projects page

---
+++ src/config/projectConfig.js
@@ -0,0 +1,15 @@
+class ProjectConfig {
+  headerText = "Imagination Trumps Knowledge!";
+  informationText = [
+    {
+      title: "Crypto Screener Application",
+      text: "A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency.",
+    },
+    {
+      title: "React Portfolio Website",
+      text: "A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth page transitions, cool background effects, unique design and it is mobile responsive.",
+    },
+  ];
+}
+
+export const projectConfig = new ProjectConfig();
