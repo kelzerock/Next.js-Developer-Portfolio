511ff4e9ff9aaf11c9792f8605318d89e7b2432d
Aleksei Zhuchkov <kelzzzer@gmail.com>
2024-11-03 16:50:30 +0300

    feat: add file articles config

---
+++ src/config/articlesConfig.js
@@ -0,0 +1,38 @@
+class ArticlesConfig {
+  headerText = "Words Can Change The World!";
+  fullArticles = [
+    {
+      title: "Build A Custom Pagination Component In Reactjs From Scratch",
+      text: "Learn how to build a custom pagination component in ReactJS from scratch. Follow this step-by-step guide to integrate Pagination component in your ReactJS project.",
+      time: "9 min read",
+    },
+    {
+      title:
+        "Creating Stunning Loading Screens In React: Build 3 Types Of Loading Screens",
+      text: "Learn how to create stunning loading screens in React with 3 different methods. Discover how to use React-Loading, React-Lottie & build a custom loading screen. Improve the user experience.",
+      time: "10 min read",
+    },
+  ];
+  shortArticles = [
+    {
+      title:
+        "Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling",
+    },
+    {
+      title:
+        "Silky Smooth Scrolling In Reactjs: A Step-By-Step Guide For React Developers",
+    },
+    {
+      title:
+        "Creating An Efficient Modal Component In React Using Hooks And Portals",
+    },
+    {
+      title:
+        "Build A Fabulous Todo List App With React, Redux And Framer-Motion",
+    },
+    { title: "Redux Simplified: A Beginner's Guide For Web Developers" },
+    { title: "What Is Higher Order Component (Hoc) In React?" },
+  ];
+}
+
+export const articlesConfig = new ArticlesConfig();
